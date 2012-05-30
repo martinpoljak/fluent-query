fba2cb441443f98fe574b0ac365380517f292803
Martin Kozák <martinkozak@martinkozak.net>
2012-05-30 13:30:21 +0200

    optimizations; data aren't further the Hashie::Mash

--- lib/fluent-query/data.rb
+++ lib/fluent-query/data.rb
@@ -9,14 +9,44 @@ module FluentQuery
      # access its fields by "object way".
      #
      
-     class Data < ::Hash
+     class Data
+     
+        ##
+        # Contained data hash.
+        #
+        
+        @data
+     
+        ## 
+        # Constructor.
+        #
+        
+        def initialize(data)
+            @data = data
+        end
      
         ##
         # Maps missing calls to data elements.
         #
         
-        def method_missing(name)
-            self[name]
+        def method_missing(name, *args)
+            @data[name]
+        end
+        
+        ##
+        # Maps array access to underlying data object.
+        #
+        
+        def [](key)
+            @data[key.to_sym]
+        end
+        
+        ##
+        # Converts data to hash.
+        #
+        
+        def to_hash
+            @data.to_hash.dup
         end
         
      end
